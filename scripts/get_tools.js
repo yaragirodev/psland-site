1367aaa39242138f5197e6793299764c9be0de4c
Yaroslav <116502429+yaragirodev@users.noreply.github.com>
2026-03-17 00:21:58 +0300

    adding tools fetch from json

---
+++ scripts/get_tools.js
@@ -0,0 +1,21 @@
+fetch('list/list_tools.json')
+  .then(response => response.json())
+  .then(tools => {
+    const container = document.getElementById('cards-container');
+
+    tools.forEach(tool => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      card.innerHTML = `
+        <h2>${tool.title}</h2>
+        <p>${tool.description}</p>
+        <a href="${tool.link}">Открыть</a>
+      `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(error => {
+    console.error('error parsing JSON:', error);
+  });
